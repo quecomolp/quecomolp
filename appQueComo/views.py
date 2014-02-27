31d3906327e11db352920cc52a5539d5e6711a29
Belen Dominguez <belendominguez@gmail.com>
2014-02-27 12:17:12 -0300

    primer push

---
+++ appQueComo/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, render_to_response
+
+def index (request):
+	return render_to_response ('default/index.html')
+
+def registro (request):
+	return render_to_response('default/register.html')
