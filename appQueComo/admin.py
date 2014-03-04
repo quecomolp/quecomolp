2d6c522aee257b61d5d8f7843034fa704fef8c9f
Belen Dominguez <belendominguez@gmail.com>
2014-03-04 15:01:46 -0300

    Agregado al panel de admin

--- appQueComo/admin.py
+++ appQueComo/admin.py
@@ -1,2 +1,10 @@
 from django.contrib import admin
-from appQueComo.models import singleRestaurante
+from appQueComo.models import singleRestaurante, menu, tipoComidas, ServiciosParaOfrecer, DatosRestaurante, DatosPersonaRegistrada, ImagenComida
+
+admin.site.register(singleRestaurante)
+admin.site.register(menu)
+admin.site.register(tipoComidas)
+admin.site.register(ServiciosParaOfrecer)
+admin.site.register(DatosRestaurante)
+admin.site.register(DatosPersonaRegistrada)
+admin.site.register(ImagenComida)
